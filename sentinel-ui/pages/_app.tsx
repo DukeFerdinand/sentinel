969a60936a30b1317e28f8d9406fefeabac35376
Doug Hooker <DouglassHooker@gmail.com>
2020-11-29 21:01:18 -0600

    fix typing in _app.tsx

--- sentinel-ui/pages/_app.tsx
+++ sentinel-ui/pages/_app.tsx
@@ -1,6 +1,9 @@
+import { AppProps } from 'next/dist/next-server/lib/router/router';
+import { ReactNode } from 'react';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
+//! This return type might be wrong, so if anything screwy happens, look here
+function MyApp({ Component, pageProps }: AppProps): ReactNode {
   return <Component {...pageProps} />;
 }
 
