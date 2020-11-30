0c5312c905aac646e7dea843819c5031f321a969
Doug Hooker <DouglassHooker@gmail.com>
2020-11-29 20:39:13 -0600

    Init next js app for ui

---
+++ sentinel-ui/pages/index.tsx
@@ -0,0 +1,12 @@
+import Layout from '../components/layout';
+import styles from '../styles/Home.module.css';
+
+const Home: React.FC = () => {
+  return (
+    <Layout>
+      <div className={styles.container}>Hello!</div>
+    </Layout>
+  );
+};
+
+export default Home;
