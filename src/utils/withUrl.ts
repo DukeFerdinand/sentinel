d3fd745000f186ed4bbc40f55ffae4390c5addd9
Doug Hooker <DouglassHooker@gmail.com>
2020-12-11 21:56:37 -0600

    update withUrl and public env variables to use NEXT_PUBLIC_

--- src/utils/withUrl.ts
+++ src/utils/withUrl.ts
@@ -1,7 +1,11 @@
 export const withUrl = (uri = ''): string => {
+  console.info('BASEURL', process.env.NEXT_PUBLIC_BASEURL);
   // Any hosting site this will live on is going to provide built-in https,
   // so http should only be used on the dev side
   return `${
-    process.env.IS_LOCAL && process.env.IS_LOCAL !== 'false' ? 'http' : 'https'
-  }://${process.env.BASEURL}${uri}`;
+    process.env.NEXT_PUBLIC_IS_LOCAL &&
+    process.env.NEXT_PUBLIC_IS_LOCAL !== 'false'
+      ? 'http'
+      : 'https'
+  }://${process.env.NEXT_PUBLIC_BASEURL}${uri}`;
 };
