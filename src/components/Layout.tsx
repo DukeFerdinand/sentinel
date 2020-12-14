9e964a5686a44b339ebfd28829327c50c9ab515e
Doug Hooker <DouglassHooker@gmail.com>
2020-12-13 22:32:51 -0600

    Add USER_QUERY to pull fake user info, set to context

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,6 +3,8 @@ import { css, Global } from '@emotion/react';
 import { GlobalStyles } from '../styles/globals';
 import { AppBar } from './AppBar';
 import { Breakpoints } from '../styles/breakpoints';
+import { useContext } from 'react';
+import { UserContext } from '../store';
 
 const LayoutStyles = {
   Main: css({
@@ -15,6 +17,7 @@ const LayoutStyles = {
 };
 
 const Layout: React.FC = ({ children }) => {
+  const { user } = useContext(UserContext);
   return (
     <main css={LayoutStyles.Main}>
       <Head>
@@ -28,7 +31,7 @@ const Layout: React.FC = ({ children }) => {
       <Global styles={GlobalStyles} />
 
       {/* Then any top level components */}
-      <AppBar />
+      {user && <AppBar />}
 
       {/* THEN the main flow */}
       {children}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 import { NextPage } from 'next';
-import { SmartFetch } from '@dukeferdinand/ts-utils';
 import { gql, useQuery } from '@apollo/react-hooks';
 
 import { withApollo } from '../lib/apollo';
-
-const { smartFetch, RequestMethods } = SmartFetch;
+import { User } from '../@generated/graphql';
+import { useContext } from 'react';
+import { UserContext } from '../store';
 
 interface HomeProps {
   data?: {
@@ -21,14 +21,34 @@ export const HELLO_QUERY = gql`
   }
 `;
 
-export const Home: NextPage<HomeProps> = () => {
-  const { data, loading, error } = useQuery<{ sayHello: string }>(HELLO_QUERY);
+export const USER_QUERY = gql`
+  query UserQuery {
+    user(id: "uuid") {
+      id
+      username
+    }
+  }
+`;
 
-  if (!loading) {
-    return <div>{data?.sayHello || 'Error'}</div>;
-  } else {
+export const Home: NextPage<HomeProps> = () => {
+  const { data, loading, error } = useQuery<{ user: User }>(USER_QUERY);
+
+  const { user, setUser } = useContext(UserContext);
+
+  if (error) {
+    console.error(error);
+  }
+
+  if (loading) {
     return <div>Loading</div>;
   }
+
+  if (data) {
+    setUser && setUser(data.user);
+    return <div>Hello, fake user {user?.username}!</div>;
+  }
+
+  return <div>Got an error, check console.</div>;
 };
 
 export default withApollo(Home);
