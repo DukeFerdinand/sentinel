e02ed224c00cce4c16bd861629fdc20947f1c8b8
Doug Hooker <DouglassHooker@gmail.com>
2020-12-21 16:19:59 -0600

    Re-arrange ProjectLayout to expose dispatch option

--- src/components/Projects/ProjectLayout.tsx
+++ src/components/Projects/ProjectLayout.tsx
@@ -1,10 +1,16 @@
 import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
 import Head from 'next/head';
+import { Fragment, useContext, useEffect } from 'react';
 
 import { Project } from '../../@generated/graphql';
 import { authClient } from '../../lib/apolloClient';
-import { ProjectStateProvider } from '../../store/projects';
+import { ProjectAction } from '../../store/actions';
+import {
+  ProjectState,
+  ProjectStateProvider,
+  ProjectStore,
+} from '../../store/projects';
 import { ProjectManagementBar } from './ProjectManagementBar';
 import { ProjectSidebar } from './ProjectSidebar';
 
@@ -24,11 +30,10 @@ const ALL_PROJECTS_QUERY = gql`
   }
 `;
 
-export const ProjectLayout: React.FC<ProjectLayoutProps> = ({
-  children,
-  showManagementBar,
-  title,
-}) => {
+const ProjectLayoutWithState: React.FC<
+  Pick<ProjectLayoutProps, 'showManagementBar'>
+> = ({ children, showManagementBar }) => {
+  const { project, dispatch } = useContext(ProjectStore);
   const { data, loading, error } = useQuery<{ projects: Array<Project> }>(
     ALL_PROJECTS_QUERY,
     {
@@ -36,20 +41,44 @@ export const ProjectLayout: React.FC<ProjectLayoutProps> = ({
     }
   );
 
+  useEffect(() => {
+    if (data) {
+      dispatch({
+        type: ProjectAction.INIT_STATE,
+        payload: {
+          projects: data.projects,
+          // Eventually we should enable a default selection
+          project: project || data.projects[0],
+        } as ProjectState,
+      });
+    }
+  }, [data, dispatch, project]);
+
+  return (
+    <Fragment>
+      <ProjectSidebar />
+      <div className="flex-grow">
+        {showManagementBar && <ProjectManagementBar />}
+        <div className="px-10 py-3">{children}</div>
+      </div>
+    </Fragment>
+  );
+};
+
+export const ProjectLayout: React.FC<ProjectLayoutProps> = ({
+  children,
+  showManagementBar,
+  title,
+}) => {
   return (
     <section className="h-full flex flex-row">
       <Head>
         <title>{title}</title>
       </Head>
-      <ProjectStateProvider
-        project={data?.projects[0] || undefined}
-        projects={data?.projects || []}
-      >
-        <ProjectSidebar />
-        <div className="flex-grow">
-          {showManagementBar && <ProjectManagementBar />}
-          <div className="px-10 py-3">{children}</div>
-        </div>
+      <ProjectStateProvider project={undefined} projects={[]}>
+        <ProjectLayoutWithState showManagementBar={showManagementBar}>
+          {children}
+        </ProjectLayoutWithState>
       </ProjectStateProvider>
     </section>
   );
