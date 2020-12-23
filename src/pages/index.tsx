95f3f0ed3424937a5598c72f5721d8db7d3bdde6
Doug Hooker <DouglassHooker@gmail.com>
2020-12-22 23:04:24 -0600

    Temp fix some mobile styles breaking

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,8 +21,10 @@ export const Home: NextPage = () => {
         <div>nice</div>
       </section> */}
       <section className="relative h-full flex flex-col lg:flex-row items-center">
-        <div className="h-full flex items-center justify-center w-full lg:w-1/2 bg-gradient-to-br from-green-400 to-blue-500">
-          <div className="w-3/4">
+        {/* Code picture container */}
+        <div className="h-full flex items-center justify-center py-20 px-4 w-full lg:w-1/2 bg-gradient-to-br from-green-400 to-blue-500">
+          {/* Wrapper to position image */}
+          <div className="w-full lg:w-3/4">
             <img
               css={HomeStyles.Image}
               className="rounded-xl overflow-hidden shadow-md mb-2"
@@ -37,22 +39,29 @@ export const Home: NextPage = () => {
         <div className="flex flex-col z-10 p-8 w-full lg:w-1/2 border shadow-md h-full ml-auto">
           <h1 className="mb-2 text-4xl text-black">Welcome to Sentinel!</h1>
           <p>The open source logger and error tracker</p>
+          <p>Links here</p>
+
           {/* Carousel */}
-          <div className="m-auto lg:w-3/4">
-            <h3 className="text-2xl">Features</h3>
+          <h3 className="mt-auto mb-4 text-3xl">Features</h3>
+          <div className="m-auto mt-4 lg:w-3/4">
             <Carousel className="h-80">
-              <div className="bg-blue-800 h-80"></div>
+              <div className="bg-blue-800 h-80 flex">
+                <p className="text-white m-auto w-3/4">
+                  Last feature page: This application is a work in progress, and
+                  OPEN SOURCE! So if you don't see a feature you want, you can
+                  either make an issue and wait for me to roll it out, or make a
+                  PR ;)
+                  <br />
+                  <br />
+                  {'<link to the contribution page>'}
+                </p>
+              </div>
             </Carousel>
-            <p>
-              This application is a work in progress, and OPEN SOURCE! So if you
-              don't see a feature you want, you can either wait for it to roll
-              out or make a PR ;)
-            </p>
           </div>
-          <div className="mt-auto flex items-center justify-end">
+          <div className="mt-4 flex items-center justify-end">
             <b>TL;DR</b>:{' '}
             <Link href="/getting-started">
-              <a className="mx-5 text-white border shadow-sm px-5 py-3 bg-gradient-to-br rounded-md from-green-400 to-blue-500">
+              <a className="mx-5 text-white border shadow-sm px-5 py-3 bg-gradient-to-br rounded-md from-green-400 to-blue-500 whitespace-nowrap">
                 Get Started
               </a>
             </Link>
