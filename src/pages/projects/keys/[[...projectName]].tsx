0853d58b5b5e32e1fc7b96a9d82dfa198d9840a6
Doug Hooker <DouglassHooker@gmail.com>
2020-12-26 17:13:04 -0600

    Tie in api key form

--- src/pages/projects/keys/[[...projectName]].tsx
+++ src/pages/projects/keys/[[...projectName]].tsx
@@ -1,14 +1,41 @@
-import { NextPage } from 'next';
-import { useRouter } from 'next/dist/client/router';
 import { useContext, useEffect } from 'react';
+import { NextPage } from 'next';
+import { Field, Form, Formik } from 'formik';
+import { useRouter } from 'next/dist/client/router';
+
 import { formatProjectName } from '../../../api/utils';
 import { ProjectLayout } from '../../../components/Projects/ProjectLayout';
 import { withApollo } from '../../../lib/apollo';
 import { ProjectStore } from '../../../store/projects';
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
+import {
+  ApiKeyResponse,
+  MutationAddApiKeyArgs,
+} from '../../../@generated/graphql';
+import { authClient } from '../../../lib/apolloClient';
+
+const ADD_KEY_MUTATION = gql`
+  mutation AddKey($config: ApiKeyInput) {
+    addApiKey(config: $config) {
+      storedInfo {
+        id
+        name
+        project
+        environment
+      }
+      key
+    }
+  }
+`;
 
 const KeyManagementContent: NextPage = () => {
   const router = useRouter();
   const { projects, project } = useContext(ProjectStore);
+  const [addKey] = useMutation<ApiKeyResponse>(ADD_KEY_MUTATION, {
+    client: authClient,
+  });
+
   useEffect(() => {
     // TODO: Add setter for undefined project but a DEFINED query
 
@@ -25,26 +52,73 @@ const KeyManagementContent: NextPage = () => {
       ) : (
         <div className="flex flex-col">
           <h2 className="text-2xl mb-4">Manage API keys for {project.name}</h2>
-          <div className="border mb-4 p-4 w-full lg:w-1/2 flex flex-col rounded-md">
-            <div className="flex flex-row w-full">
-              <h3 className="text-xl">Generate a key</h3>
+          <Formik
+            initialValues={{
+              keyName: '',
+              environment: '',
+            }}
+            onSubmit={async (values, helpers) => {
+              if (!values.keyName) {
+                helpers.setFieldError('keyName', 'Key name required');
+              }
+              if (!values.environment) {
+                helpers.setFieldError(
+                  'environment',
+                  'Environment name required'
+                );
+              }
 
-              <button className="rounded border ml-auto p-1 pl-2 flex items-center justify-center hover:bg-gray-100">
-                Add a key
-                <i className="material-icons text-lg ml-1">add</i>
-              </button>
-            </div>
-            <div className="flex flex-row w-full items-center">
-              <label htmlFor="api-key-name" className="mr-4">
-                Key Name
-              </label>
-              <input
-                className="rounded-md"
-                type="text"
-                placeholder="Key Name"
-              />
-            </div>
-          </div>
+              if (values.keyName && values.environment) {
+                console.info('Ready to submit');
+                const config: MutationAddApiKeyArgs['config'] = {
+                  name: values.keyName,
+                  environment: values.environment,
+                  project: project.id,
+                  projectName: (router.query.projectName as string[])[0],
+                };
+                const result = await addKey({
+                  variables: {
+                    config,
+                  },
+                });
+
+                console.info(result);
+              }
+            }}
+          >
+            <Form className="border mb-4 p-4 w-full lg:w-1/2 flex flex-col rounded-md">
+              <div className="flex flex-row w-full">
+                <h3 className="text-xl">Generate a key</h3>
+
+                <button className="rounded border ml-auto p-1 pl-2 flex items-center justify-center hover:bg-gray-100">
+                  Add Key
+                  <i className="material-icons text-lg ml-1">add</i>
+                </button>
+              </div>
+              <div className="flex flex-row w-full items-center mb-4">
+                <label htmlFor="keyName" className="mr-4">
+                  Key Name
+                </label>
+                <Field
+                  className="rounded-md"
+                  type="text"
+                  name="keyName"
+                  placeholder="Key Name"
+                />
+              </div>
+              <div className="flex flex-row w-full items-center">
+                <label htmlFor="environment" className="mr-4">
+                  Environment
+                </label>
+                <Field
+                  className="rounded-md"
+                  type="text"
+                  name="environment"
+                  placeholder="dev, staging, prod, etc."
+                />
+              </div>
+            </Form>
+          </Formik>
           {/* Render Keys */}
           <div className="flex flex-col pt-4">
             <h2 className="text-2xl mb-4">Active Keys</h2>
